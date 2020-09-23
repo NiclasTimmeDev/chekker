c0352b0165c422e9b8dc1067e075032fec405f94
NiclasTimmeDev <niclastimmdev@gmail.com>
2020-09-23 22:40:46 +0200

    small bug fix

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // Procecces.
-Route::resource('processes', 'ProcessesController');
+Route::resource('processes', 'ProcessController');
