ceeabd0926dbc84a6ebe70a529fd7a32a9bee3e1
NiclasTimmeDev <niclastimmdev@gmail.com>
2020-10-18 16:47:51 +0200

    Implement small AdminSidebar

--- client/src/components/AdminSidebar/AdminSidebar.vue
+++ client/src/components/AdminSidebar/AdminSidebar.vue
@@ -1,28 +1,30 @@
 <template>
-    <aside
-        id="admin-sidebar"
-        class="sidebar bg-darkest"
-        :class="{ 'sidebar-big': sidebarBig, 'sidebar-small': !sidebarBig }"
-    >
-        <div class="icon-toggle-sidebar px-3">
-            <i
-                class="fas fa-angle-double-left"
-                :class="{ rotate: !sidebarBig }"
-                @click="toggleSidebarWidth"
-            ></i>
-        </div>
+    <aside id="admin-sidebar" class="sidebar bg-darkest">
+        <div class="sidebar-content pt-2 pb-3 px-1">
+            <!-- ADD BUTTON -->
+            <div @click="toggleModal">
+                <AdminSidebarIcon
+                    :class="'fas fa-plus'"
+                    :isButtonIcon="true"
+                ></AdminSidebarIcon>
+            </div>
 
-        <div class="sidebar-content pt-2 pb-3 px-3">
-            <!-- ACTION BUTTON -->
-            <button
-                class="btn btn-primary btn-block mx-auto"
-                @click="toggleModal"
-            >
-                <i class="fas fa-plus"></i> Neu
-            </button>
+            <!-- PROFILE -->
+            <router-link to="/profile">
+                <AdminSidebarIcon :class="'fas fa-user'"></AdminSidebarIcon>
+            </router-link>
 
-            <!-- SELECT TEAM -->
-            <admin-team-display></admin-team-display>
+            <!-- CHECKMARK -->
+            <router-link to="/process/view">
+                <AdminSidebarIcon :class="'fas fa-check'"></AdminSidebarIcon>
+            </router-link>
+
+            <!-- MORE -->
+            <router-link to="/process/view">
+                <AdminSidebarIcon
+                    :class="'fas fa-ellipsis-h'"
+                ></AdminSidebarIcon>
+            </router-link>
 
             <!-- MODAL -->
             <create-new-modal :showModal="showModal" :toggleModal="toggleModal">
@@ -51,75 +53,6 @@
                     </ul>
                 </template>
             </create-new-modal>
-
-            <!-- PROCESSES -->
-            <admin-control :showChevron="true">
-                <template v-slot:icon>
-                    <i class="fas fa-check"></i>
-                </template>
-                <template v-slot:title>
-                    Prozesse
-                </template>
-                <template v-slot:content>
-                    <ul>
-                        <li class="mt-3 mb-3">
-                            <router-link to="/process/view"
-                                >Alle Prozesse</router-link
-                            >
-                        </li>
-                    </ul>
-                </template>
-            </admin-control>
-
-            <!-- TEAM -->
-            <admin-control :showChevron="true">
-                <template v-slot:icon>
-                    <i class="fas fa-users"></i>
-                </template>
-                <template v-slot:title>
-                    Team
-                </template>
-                <template v-slot:content>
-                    <ul>
-                        <li class="mt-3 mb-3">
-                            <router-link to="/team">Teammitglieder</router-link>
-                        </li>
-                        <li class="mb-3">
-                            <router-link to="/team/join"
-                                >Team beitreten</router-link
-                            >
-                        </li>
-                    </ul>
-                </template>
-            </admin-control>
-
-            <!-- ACTIVE PROCESSES -->
-            <admin-control :showChevron="true">
-                <template v-slot:icon>
-                    <i class="fas fa-clock"></i>
-                </template>
-                <template v-slot:title>
-                    Aktive Prozesse
-                </template>
-                <template v-slot:content>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-                    aliquam saepe ratione adipisci! At recusandae quae voluptas
-                    iure distinctio! At alias quis enim nesciunt quas beatae
-                    laborum earum, optio sed!
-                </template>
-            </admin-control>
-
-            <!-- SETTINGS -->
-            <admin-control>
-                <template v-slot:icon>
-                    <i class="fas fa-cog"></i>
-                </template>
-                <template v-slot:title>
-                    <router-link :to="{ name: 'Settings' }"
-                        >Einstellungen</router-link
-                    >
-                </template>
-            </admin-control>
         </div>
     </aside>
 </template>
@@ -127,6 +60,7 @@
 import AdminControl from "./AdminControl.vue";
 import Modal from "./../UI/Modal";
 import AdminTeamDisplay from "./AdminTeamDisplay.vue";
+import AdminSidebarIcon from "./AdminSidebarIcon";
 export default {
     data() {
         return {
@@ -137,11 +71,13 @@ export default {
     components: {
         "admin-control": AdminControl,
         "create-new-modal": Modal,
-        "admin-team-display": AdminTeamDisplay
+        "admin-team-display": AdminTeamDisplay,
+        AdminSidebarIcon: AdminSidebarIcon
     },
     methods: {
         toggleModal() {
             this.showModal = !this.showModal;
+            console.log("ok");
         },
         toggleSidebarWidth() {
             this.sidebarBig = !this.sidebarBig;
---
+++ client/src/components/AdminSidebar/AdminSidebarIcon.vue
@@ -0,0 +1,19 @@
+<template>
+    <!--
+    The icon that is shown is the sidebar.
+    It's a round circle with with a font awesome icon inside
+    that accepts a click event.
+    -->
+    <div
+        class="sidebar-icon mb-4 mx-auto"
+        :class="{ 'button-icon': isButtonIcon }"
+    >
+        <!-- ICON -->
+        <i :class="icon"></i>
+    </div>
+</template>
+<script>
+export default {
+    props: ["click", "icon", "isButtonIcon"]
+};
+</script>
